b7cf1593bf0710c09da8b00a28748f221a254580
yujade_01 <yujade.saik@innity.com>
2025-05-08 14:22:08 +0800

    update code

--- timegpt_app.py
+++ timegpt_app.py
@@ -20,52 +20,57 @@ if nixtla_api_key:
     uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
     
     if uploaded_file is not None:
-        # bytes_data = uploaded_file.read()
-        # data_str = bytes_data.decode('utf-8')
-        # Read file from byte data
-        df = pd.read_csv(uploaded_file, encoding='unicode_escape') # skiprows=6 (skip first 6 rows)
-        df.rename(columns={"ORDERDATE":"Date"}, inplace=True)
-        # Convert to datetime format
-        print(df.columns)
-        df["Date"] = pd.to_datetime(df["Date"])
+        df = pd.read_csv(uploaded_file, encoding='unicode_escape')
+        if "Date" or "date" in df.columns.tolist():
+            # Convert to datetime format
+            print(df.columns)
+            df["Date"] = pd.to_datetime(df["Date"])
 
-        d = st.date_input(
-            "Select your date range as historical data: ",
-            (pd.to_datetime(df["Date"].min()), pd.to_datetime(df["Date"].max())),
-            format="MM.DD.YYYY",
-        )
-        if d:
-            min_date = str(d[0])
-            max_date = str(d[1])
-            df_filter = df.loc[(df["Date"] >= min_date) & (df["Date"] <= max_date)]
-            # Preview selected data
-            st.write("Selected Data preview:")
-            st.write(df_filter)
-            # st.write(df_filter["Daily minimum temperatures"].dtypes)
-            df_filter['Daily minimum temperatures'] = pd.to_numeric(df_filter['Daily minimum temperatures'], errors='coerce')
-            date_range = pd.date_range(start=min_date, end=max_date, freq='D')
-            date_df = pd.DataFrame({'Date': date_range})
+            d = st.date_input(
+                "Select your date range as historical data: ",
+                (pd.to_datetime(df["Date"].min()), pd.to_datetime(df["Date"].max())),
+                format="MM.DD.YYYY",
+            )
+            if d:
+                min_date = str(d[0])
+                max_date = str(d[1])
+                df_filter = df.loc[(df["Date"] >= min_date) & (df["Date"] <= max_date)]
+                date_range = pd.date_range(start=min_date, end=max_date, freq='D')
+                date_df = pd.DataFrame({'Date': date_range})
 
-            # Merge with Original Data
-            df_merged = pd.merge(date_df, df_filter, on='Date', how='left')
-            df_merged['Daily minimum temperatures'] = df_merged['Daily minimum temperatures'].fillna(0)
-            # Select numeric columns
-            column_list = df_merged.select_dtypes(include=np.number).columns.tolist()
-            TARGET = st.selectbox("Select Target variable", column_list)
-            forecast_horizon = st.number_input("Forecast horizon", value=None, min_value=1, max_value=365)
+                # Merge with Original Data
+                df_merged = pd.merge(date_df, df_filter, on='Date', how='left')
+                # Select numeric columns
+                column_list = df_merged.select_dtypes(include=np.number).columns.tolist()
+                target = st.selectbox("Select Target variable", [""] + column_list)
+                if target:
+                    df_merged = df_merged[["Date", target]]
+                    df_merged[target] = df_merged[target].fillna(0)
+                    # Preview selected data
+                    st.write("Selected Data preview:")
+                    st.write(df_merged.head(10))
+                    group_method = st.selectbox("Select groupby aggregate method: ", ["Sum", "Mean"])
+                    if group_method == "Sum":
+                        df_merged = df_merged.groupby('Date').sum().reset_index()
+                    else:
+                        df_merged = df_merged.groupby('Date').mean().reset_index()
 
-        if TARGET and forecast_horizon is not None:
-            df_processed = df_merged.copy()
-            df_processed["unique_id"] = 1
-            df_processed = df_processed[["unique_id", "Date", TARGET]]
-            df_processed = df_processed.rename(columns={"Date":"ds",
-                                    TARGET:"y"})
-            timegpt_fcst_df = nixtla_client.forecast(df=df_processed, h=forecast_horizon, freq='D', time_col='ds', target_col='y')
-            if df_processed.y.dtypes == "int64":
-                timegpt_fcst_df["TimeGPT"] = round(timegpt_fcst_df["TimeGPT"])
-            
-            st.write("TimeGPT forecast result:")
-            st.write(timegpt_fcst_df)
-            st.write("Plot:")
-            st.write(nixtla_client.plot(df_processed, timegpt_fcst_df, time_col='ds', target_col='y'))
-    
+                    df_merged = df_merged.drop_duplicates(subset=["Date"])
+                    forecast_horizon = st.number_input("Forecast horizon", value=None, min_value=1, max_value=365)
+
+            if target and forecast_horizon is not None:
+                df_processed = df_merged.copy()
+                df_processed["unique_id"] = 1
+                df_processed = df_processed[["unique_id", "Date", target]]
+                df_processed = df_processed.rename(columns={"Date":"ds",
+                                        target:"y"})
+                timegpt_fcst_df = nixtla_client.forecast(df=df_processed, h=forecast_horizon, freq='D', time_col='ds', target_col='y')
+                if df_processed.y.dtypes == "int64":
+                    timegpt_fcst_df["TimeGPT"] = round(timegpt_fcst_df["TimeGPT"])
+                
+                st.write("TimeGPT forecast result:")
+                st.write(timegpt_fcst_df)
+                st.write("Plot:")
+                st.write(nixtla_client.plot(df_processed, timegpt_fcst_df, time_col='ds', target_col='y'))
+        else:
+            st.info("No date columns in the csv file")
